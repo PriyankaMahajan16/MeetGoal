e6deef4e8bc63743376ca26d79fdf8fa41ca76ce
Priyanka Mahajan <pm01.03.14@gmail.com>
2024-08-20 16:30:12 +0530

    Basic UI and Functionality Added.
    Lot to be done!

---
+++ javaScript.js
@@ -0,0 +1,33 @@
+var addButton = document.querySelector('#item1');
+var itemInput = document.querySelector('#new-item-input');
+var defaultInput = document.querySelector('#home-collapse');
+
+// Function to add a new item to the end of the list
+function addNewItemToEnd() {
+    if(itemInput.style.visibility=="visible")
+    {
+        itemInput.style.visibility="hidden";
+        return;
+    }
+    itemInput.style.visibility="visible";
+  }
+
+// Optional: Add an event listener for the Enter key in the input field
+itemInput.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') 
+        {
+        // Get the value from the input field
+        var newItemValue = itemInput.value.trim(); 
+
+        var list = document.querySelector('#ul1');
+        var newItem = document.createElement('li');
+        newItem.textContent = newItemValue;
+        list.appendChild(newItem);
+    
+        itemInput.style.visibility="hidden";
+        addButton.classList.remove("collapsed");
+        defaultInput.classList.add("show");
+    }});
+
+// Add an event listener to the 'Add Item' button
+//addButton.addEventListener('onclick', addNewItemToEnd('MangoAdded'));
